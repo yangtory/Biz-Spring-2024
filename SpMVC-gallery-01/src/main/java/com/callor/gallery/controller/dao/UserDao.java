cf39487ed2706f7c378943eb7dfc156f187f32b3
yangtory <wjdduscldrn@naver.com>
2024-04-18 12:52:44 +0900

    bbs

---
+++ SpMVC-gallery-01/src/main/java/com/callor/gallery/controller/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.callor.gallery.controller.dao;
+
+public interface UserDao {
+
+}
---
+++ SpMVC-gallery-01/src/main/webapp/static/js/main_nav.js
@@ -0,0 +1,25 @@
+// main-nav 각 tag className 을 키로하는 객체 선언
+const NAV_INDEX = {
+    home: { url: '/' },
+    notice: { url: '/bbs/notice' },
+    bbs: { url: '/bbs/free' },
+};
+document.addEventListener('DOMContentLoaded', () => {
+    const mainNav = document.querySelector('nav.main');
+    const navItems = mainNav.querySelectorAll('LI');
+
+    mainNav.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.tagName === 'LI') {
+            // 모든 nav.main li tag 에 active 라는 class 를 제거하기
+            navItems.forEach((item) => {
+                item.classList.remove('active');
+            });
+            target.classList.add('active');
+
+            const className = target.classList[0];
+            const url = NAV_INDEX[className].url;
+            document.location.href = `${rootPath}${url}`;
+        }
+    });
+});
